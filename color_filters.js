e7f3a1de9ebaad9a3a1b15da1d5c4873baa0cbac
Hyunill Kang <hyunillkang@gmail.com>
2019-05-12 02:10:25 -0400

    image filter

--- color_filters.js
+++ color_filters.js
@@ -23,10 +23,10 @@ function applySepia() {
 
         imageData.data[i] = 0.393 * imageData.data[i] + 0.769 * imageData.data[i + 1] + 0.189 * imageData.data[i + 2];
         imageData.data[i] = imageData.data[i] > 255 ? 255 : parseInt(imageData.data[i]);
-        
+
         imageData.data[i + 1] = 0.349 * imageData.data[i] + 0.686 * imageData.data[i + 1] + 0.168 * imageData.data[i + 2];
         imageData.data[i + 1] = imageData.data[i + 1] > 255 ? 255 : parseInt(imageData.data[i + 1]);
-        
+
         imageData.data[i + 2] = 0.272 * imageData.data[i] + 0.534 * imageData.data[i + 1] + 0.131 * imageData.data[i + 2];
         imageData.data[i + 2] = imageData.data[i + 2] > 255 ? 255 : parseInt(imageData.data[i + 2]);
 
@@ -46,8 +46,8 @@ function applyInvert() {
     for (var i = 0; i < imageData.data.length; i += 4) {
 
         imageData.data[i] = 255 - imageData.data[i];
-        imageData.data[i + 1] = 255 - imageData.data[i+1];
-        imageData.data[i + 2] = 255 - imageData.data[i+2];
+        imageData.data[i + 1] = 255 - imageData.data[i + 1];
+        imageData.data[i + 2] = 255 - imageData.data[i + 2];
         //        imageData.data[i+3] = imageData.data[i+3];
 
     }
@@ -58,12 +58,12 @@ function applyInvert() {
 function adjustRGBAChannel() {
 
 
-    let imageSlider = document.getElementsByName("imageSlider");
+    let rgbaSlider = document.getElementsByName("rgbaSlider");
 
-    let redSlider = imageSlider[0];
-    let greenSlider = imageSlider[1];
-    let blueSlider = imageSlider[2];
-    let alphaSlider = imageSlider[3];
+    let redSlider = rgbaSlider[0];
+    let greenSlider = rgbaSlider[1];
+    let blueSlider = rgbaSlider[2];
+    let alphaSlider = rgbaSlider[3];
 
     let convertedData = context.createImageData(imageData.width, imageData.height);
     //    let pixels = imageData.data;
@@ -87,3 +87,83 @@ function applyRGBAChannel() {
     insertImageData();
     imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 }
+
+function adjustBrightness() {
+
+    let brightnessSlider = document.getElementsByName("brightnessSlider");
+
+    let brightness = brightnessSlider[0].value / 50;
+
+    let convertedData = context.createImageData(imageData.width, imageData.height);
+
+    for (var i = 0; i < imageData.data.length; i += 4) {
+
+        convertedData.data[i] = imageData.data[i] * brightness;
+        convertedData.data[i + 1] = imageData.data[i + 1] * brightness;
+        convertedData.data[i + 2] = imageData.data[i + 2] * brightness;
+        convertedData.data[i + 3] = imageData.data[i + 3];
+
+    }
+
+
+    context.putImageData(convertedData, 0, 0);
+}
+
+function adjustHSBChannel() {
+    let hsbSlider = document.getElementsByName("hsbSlider");
+
+    let hueSlider = hsbSlider[0];
+    let saturationSlider = hsbSlider[1];
+    let valueSlider = hsbSlider[2];
+
+    let hue = hueSlider.value;
+    let saturation = saturationSlider.value / 100;
+    let value = valueSlider.value / 100;
+
+    //let a = (1 - Math.abs(2*value - 1)) * saturation;
+    let a = value * saturation;
+    console.log(a);
+    let b = a * (1 - Math.abs((hue / 60) % 2 - 1));
+    console.log(b);
+    //    let c = value - a/2;
+    let c = value - a;
+
+    let tempR = 0;
+    let tempG = 0;
+    let tempB = 0;
+
+    if ((0 <= hue && hue < 60) || hue == 360) {
+        tempR = a;
+        tempG = b;
+    } else if (60 <= hue && hue < 120) {
+        tempR = b;
+        tempG = a;
+    } else if (120 <= hue && hue < 180) {
+        tempG = a;
+        tempB = b;
+    } else if (180 <= hue && hue < 240) {
+        tempG = b;
+        tempB = a;
+    } else if (240 <= hue && hue < 300) {
+        tempR = b;
+        tempG = a;
+    } else if (300 <= hue && hue < 360) {
+        tempR = a;
+        tempG = b;
+    }
+
+
+    let convertedData = context.createImageData(imageData.width, imageData.height);
+
+    for (var i = 0; i < imageData.data.length; i += 4) {
+
+        convertedData.data[i] = (tempR + c) * imageData.data[i]
+        convertedData.data[i + 1] = (tempG + c) * imageData.data[i + 1]
+        convertedData.data[i + 2] = (tempB + c) * imageData.data[i + 2]
+        convertedData.data[i + 3] = imageData.data[i + 3];
+
+    }
+
+
+    context.putImageData(convertedData, 0, 0);
+} 
